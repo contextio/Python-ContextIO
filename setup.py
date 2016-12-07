2b8ed9796e33f90ed114762e3793a1587e355e96
Cecy Correa <cecy.correa@returnpath.com>
2016-12-07 10:44:00 -0600

    fixing 1 to 0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 requires=['rauth', 'six']
 
 setup(name='contextio',
-    version='v1.12.1',
+    version='v1.12.0',
     description='Library for accessing the Context.IO API (v2.0 and Lite) in Python',
     author='Alex Tanton, Cecy Correa',
     author_email='alex.tanton@returnpath.com, cecy.correa@returnpath.com',
