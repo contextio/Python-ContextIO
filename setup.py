12e3fe95b57f16b8019767eb724756d1de9d5f02
Cecy Correa <cecy.correa@returnpath.com>
2016-12-07 10:12:55 -0600

    upped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 requires=['rauth', 'six']
 
 setup(name='contextio',
-    version='v1.11.4',
+    version='v1.12.1',
     description='Library for accessing the Context.IO API (v2.0 and Lite) in Python',
     author='Alex Tanton, Cecy Correa',
     author_email='alex.tanton@returnpath.com, cecy.correa@returnpath.com',
