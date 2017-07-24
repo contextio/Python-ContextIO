e1e3c806e58a6c368ae36146955424e49bad3c53
Alex Tanton <alex.tanton@returnpath.com>
2017-07-24 16:20:27 -0500

    run setup function in test && fix encoding issues

--- contextio/lib/app.py
+++ contextio/lib/app.py
@@ -7,26 +7,25 @@ from .helpers import sanitize_params
 class App(Api):
     """Application level endpoints: These apply to both Lite and 2.0 versions. To use application level endpoints, you must instantiate an object using "app" as your API version.
     """
-
     def get_webhook_logs(self, **params):
         """Webhook logs
 
         Documentation: https://docs.context.io/#logs
-        
+
         Optional arguments:
-        account: string — Only include logs of webhooks from the specified account id
-        webhook_id: string — Only include logs of webhooks from the specified webhook id
-        timestamp_before: integer (unix timestamp) — Only include logs of webhooks before a given timestamp.
-        timestamp_after: integer (unix timestamp) — Only include logs of webhooks after a given timestamp.
-        status_code_from: integer — Only return webhook logs where the status code is greater than the number specified. For example 200)
-        status_code_to: integer — Only return webhook logs where the status code is less than the number specified. For example 300)
+        account: string - Only include logs of webhooks from the specified account id
+        webhook_id: string - Only include logs of webhooks from the specified webhook id
+        timestamp_before: integer (unix timestamp) - Only include logs of webhooks before a given timestamp.
+        timestamp_after: integer (unix timestamp) - Only include logs of webhooks after a given timestamp.
+        status_code_from: integer - Only return webhook logs where the status code is greater than the number specified. For example 200)
+        status_code_to: integer - Only return webhook logs where the status code is less than the number specified. For example 300)
 
         Returns: A list of webhook logs
         """
 
         all_args = ["account", "webhook_id", "timestamp_before", "timestamp_after", "status_code_from", "status_code_to"]
         params = sanitize_params(params, all_args)
-        return [o for obj in self._request_uri("logs/webhooks", params=params)]
+        return [obj for obj in self._request_uri("logs/webhooks", params=params)]
 
     def get_call_logs(self, **params):
         """Call logs
@@ -34,21 +33,21 @@ class App(Api):
         Documentation: https://docs.context.io/#logs
 
         Optional arguments:
-        
-        account: string — Only include logs of webhooks from the specified account id
-        timestamp_before: integer (unix timestamp) — Only include logs of calls before a given timestamp.
-        timestamp_after: integer (unix timestamp) — Only include logs of calls after a given timestamp.
-        status_code_from: integer — Only return logs of calls where the status code is greater than the number specified. For example 200)
-        status_code_to: integer — Only return logs of call where the status code is less than the number specified. For example 500)
-        request_method: string — Only return logs of calls where the http method is the one specified. For example, GET
-        request_path: string — Only return logs of calls where the request matches this search string. To use regular expressions instead of simple string matching, make sure the string starts and ends with /
+
+        account: string - Only include logs of webhooks from the specified account id
+        timestamp_before: integer (unix timestamp) - Only include logs of calls before a given timestamp.
+        timestamp_after: integer (unix timestamp) - Only include logs of calls after a given timestamp.
+        status_code_from: integer - Only return logs of calls where the status code is greater than the number specified. For example 200)
+        status_code_to: integer - Only return logs of call where the status code is less than the number specified. For example 500)
+        request_method: string - Only return logs of calls where the http method is the one specified. For example, GET
+        request_path: string - Only return logs of calls where the request matches this search string. To use regular expressions instead of simple string matching, make sure the string starts and ends with /
 
         Returns: A list of call logs
         """
 
         all_args = ["account", "webhook_id", "timestamp_before", "timestamp_after", "status_code_from", "status_code_to", "request_method", "request_path"]
         params = sanitize_params(params, all_args)
-        return [o for obj in self._request_uri("logs/calls", params=params)]
+        return [obj for obj in self._request_uri("logs/calls", params=params)]
 
     def post_status_callback_url(self, **params):
         """Post an application level status_callback_url
@@ -57,7 +56,7 @@ class App(Api):
 
         Required argument:
 
-        status_callback_url: string (URL) — If specified, we’ll make a POST request to this URL if the connection status of the email account changes.
+        status_callback_url: string (URL) - If specified, we'll make a POST request to this URL if the connection status of the email account changes.
 
         Return: boolean (true / false if operation was successful)
         """
@@ -73,7 +72,7 @@ class App(Api):
 
         Arguments: none
         """
-        return [o for obj in self._request_uri("status_callback_url", params=params)]
+        return [obj for obj in self._request_uri("status_callback_url", params=params)]
 
     def delete_status_callback_url(self):
         """Delete your application status_callback_url
@@ -83,10 +82,3 @@ class App(Api):
         Arguments: none
         """
         return super(self).delete()
-
-
-
-
-
-
-
--- tests/lib/test_app.py
+++ tests/lib/test_app.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 import mock
 import unittest
 
@@ -10,10 +11,12 @@ class TestApp(unittest.TestCase):
 
     @mock.patch("contextio.lib.api.Api._request_uri")
     def test_get_webhook_logs(self, mock_request):
+        self.setUpApp()
+
         mock_request.return_value = [{"webhook_id": "some_id"}]
-        print(dir(self.api))
+
         webhook_logs = self.api.get_webhook_logs()
 
         self.assertEqual(1,len(webhook_logs))
         self.assertDictEqual({"webhook_id": "some_id"}, webhook_logs[0])
-    
+
