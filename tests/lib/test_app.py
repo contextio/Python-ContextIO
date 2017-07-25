c58c5f4558a3b6a0501012211527a33e1cc97198
Cecy Correa <cecy.correa@returnpath.com>
2017-07-25 14:18:21 -0500

    add unhappy path tests

--- tests/lib/test_app.py
+++ tests/lib/test_app.py
@@ -10,7 +10,7 @@ class TestApp(unittest.TestCase):
         self.api = App(consumer_key="foo", consumer_secret="bar", version="app")
 
     @mock.patch("contextio.lib.api.Api._request_uri")
-    def test_get_webhook_logs(self, mock_request):
+    def test_get_webhook_logs_returns_webhook_id(self, mock_request):
         self.setUpApp()
 
         mock_request.return_value = [{"webhook_id": "some_id"}]
@@ -21,7 +21,7 @@ class TestApp(unittest.TestCase):
         self.assertDictEqual({"webhook_id": "some_id"}, webhook_logs[0])
 
     @mock.patch("contextio.lib.api.Api._request_uri")
-    def test_get_call_logs(self, mock_request):
+    def test_get_call_logs_returns_http_code(self, mock_request):
         self.setUpApp()
 
         mock_request.return_value = [{"http_return_code": "code"}]
@@ -33,7 +33,7 @@ class TestApp(unittest.TestCase):
 
 
     @mock.patch("contextio.lib.api.Api._request_uri")
-    def test_post_status_callback_url(self, mock_request):
+    def test_post_status_callback_url_is_set_correctly(self, mock_request):
         self.setUpApp()
 
         mock_request.return_value = [{"success": "true"}]
@@ -44,7 +44,7 @@ class TestApp(unittest.TestCase):
         self.assertDictEqual({"success": "true"}, response[0])
 
     @mock.patch("contextio.lib.api.Api._request_uri")
-    def test_get_status_callback_url(self, mock_request):
+    def test_get_status_callback_url_returns_url(self, mock_request):
         self.setUpApp()
 
         mock_request.return_value = [{"status_callback_url": "http://callba.ck"}]
@@ -55,12 +55,22 @@ class TestApp(unittest.TestCase):
         self.assertDictEqual({"status_callback_url": "http://callba.ck"}, response[0])
 
     @mock.patch("contextio.lib.api.Api._request_uri")
-    def test_delete_status_callback_url(self, mock_request):
+    def test_delete_status_callback_url_removes_url(self, mock_request):
         self.setUpApp()
 
-        mock_request.return_value = [{"success": "true"}]
-
-        response = self.api.delete_status_callback_url()
+        mock_request.return_value = [{"error": "no status_callback_url is set"}]
+        """delete the callback, expect to get error saying there is no callback"""
+        self.api.delete_status_callback_url()
+        response = self.api.get_status_callback_url()
 
         self.assertEqual(1,len(response))
-        self.assertDictEqual({"success": "true"}, response[0])
+        self.assertDictEqual({"error": "no status_callback_url is set"}, response[0])
+
+    @mock.patch("contextio.lib.api.Api._request_uri")
+    def test_post_status_callback_url_returns_error_if_no_required_args(self, mock_request):
+        self.setUpApp()
+
+        mock_request.return_value = [{"status_callback_url": "http://callba.ck"}]
+        
+        with self.assertRaises(errors.ArgumentError):
+            self.api.post_status_callback_url()
